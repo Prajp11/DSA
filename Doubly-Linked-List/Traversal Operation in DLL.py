bb9397adcf2d64b820f64f4b733f1fcab38eb6b1
Prajwal Pandgale <pandgale.prajwal11@gmail.com>
2024-09-02 21:43:24 +0530

    Traversal Operation in DLL

---
+++ Doubly-Linked-List/Traversal Operation in DLL.py
@@ -0,0 +1,39 @@
+# Implementation of traverlsal operation in  Doubly Linked List
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.nref = None
+        self.pref = None
+
+class doublyLL:
+
+    def init (self):
+        self.head = None
+
+# Forward Traversal in DLL
+    def print_LL(self):
+        if self.head is None:
+            print("Linked List is empty!")
+
+        else:
+            n = self.head 
+            while n is not None: 
+                print(n.data, "-->",end="") 
+                n = n.nref 
+
+# Backward Traversal in DLL
+    def print_LL_reverse(self):
+        if self.head is None:
+            print("Linked List is empty!")
+
+        else:
+            n = self.head 
+            while n.ref is not None:
+                n = n.ref
+            while n is not None:
+                print(n.data, "-->",end="") 
+                n = n.pref 
+        
+dl1  = doublyLL()
+dl1.print_LL()    
