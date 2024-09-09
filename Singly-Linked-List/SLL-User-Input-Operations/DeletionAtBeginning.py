5dc00eadfc4d351df4cd68b0a70ad0a9de803c4a
Prajwal Pandgale <pandgale.prajwal11@gmail.com>
2024-09-09 14:43:33 +0530

    Deletion at beginning with user input

---
+++ Singly-Linked-List/SLL-User-Input-Operations/DeletionAtBeginning.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data, "--->", end="")
+                n = n.ref
+            print("None")  # To signify the end of the linked list
+
+    def add_begin(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+
+    def delete_begin(self):
+        if self.head is None:
+            print("Linked list is empty! Unable to delete nodes.")
+        else:
+            print(f"Deleting node with data: {self.head.data}")
+            self.head = self.head.ref
+
+# Driver Code
+LL1 = LinkedList()
+
+while True:
+    print("\nMenu:")
+    print("1. Insert at the beginning")
+    print("2. Insert at the end")
+    print("3. Delete the first node")
+    print("4. Display the linked list")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        data = int(input("Enter data to insert at the beginning: "))
+        LL1.add_begin(data)
+    elif choice == 2:
+        data = int(input("Enter data to insert at the end: "))
+        LL1.add_end(data)
+    elif choice == 3:
+        LL1.delete_begin()
+    elif choice == 4:
+        LL1.print_LL()
+    elif choice == 5:
+        break
+    else:
+        print("Invalid choice! Please try again.")
